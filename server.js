3c84fb48bac45b9d9d136fac4181f5e65f0397dd
cruzrenzcarlo <cruzrenzcarlo@gmail.com>
2023-07-26 00:01:45 -0700

    updates

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/', (req, res) => {
 
 // });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 //this is my working model end
