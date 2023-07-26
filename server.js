781bfa84476ab6568a2c597d1f87a85d7b57e49d
cruzrenzcarlo <cruzrenzcarlo@gmail.com>
2023-07-25 23:38:21 -0700

    updates

--- server.js
+++ server.js
@@ -56,7 +56,26 @@ const quiz = new mongoose.Schema({
 // Create a Mongoose model for the "records" collection
 const quizes = mongoose.model('quizes', quiz);
 
-app.get('/', async (req, res) => {
+// app.get('/', async (req, res) => {
+//   const newQuizes = {
+//     name: "Renz Cruz",
+//     sid: 300361745
+//   };
+
+//   try {
+//     // Insert the new record into the "records" collection using the Mongoose model
+//     const result = await quizes.create(newQuizes);
+//     res.send({
+//       entry: newQuizes,
+//       result
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Error occurred while inserting the record.");
+//   }
+// });
+
+app.get('/', (req, res) => {
   const newQuizes = {
     name: "Renz Cruz",
     sid: 300361745
@@ -64,7 +83,7 @@ app.get('/', async (req, res) => {
 
   try {
     // Insert the new record into the "records" collection using the Mongoose model
-    const result = await quizes.create(newQuizes);
+    const result = quizes.create(newQuizes);
     res.send({
       entry: newQuizes,
       result
@@ -79,53 +98,3 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 //this is my working model end
-
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-
-// const app = express();
-// const port = process.env.PORT || 7000;
-
-// const uri = "mongodb+srv://newuser01:1234@cluster0.7xf1ace.mongodb.net/test";
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//   console.log("MongoDB database connection established successfully");
-// });
-
-// // Middleware to parse the request body as JSON
-// app.use(express.json());
-
-// // Define a Mongoose schema for the "records" collection
-// const recordSchema = new mongoose.Schema({
-//   name: String,
-//   sid: String
-// });
-
-// // Create a Mongoose model for the "records" collection
-// const Record = mongoose.model('Record', recordSchema);
-
-// // POST route to insert a new record
-// app.post('/', async (req, res) => {
-//   const newRecord = {
-//     name: "renz",
-//     sid: "1234"
-//   };
-
-//   try {
-//     // Insert the new record into the "records" collection using the Mongoose model
-//     const result = await Record.create(newRecord);
-//     res.send({
-//       entry: newRecord,
-//       result
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("Error occurred while inserting the record.");
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
